503609a7c2027cc410e7d17f4036ccaf4d4aaac6
Boodleboy <jacobmcpeak98@gmail.com>
2025-10-06 21:56:23 -0400

    Remove now-redundant script

--- build-modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd modules
-
-../sub/wasi-sdk/build/install/bin/clang \
-  --target=wasm32-wasi \
-  --sysroot=../sub/wasi-sdk/build/install/share/wasi-sysroot \
-  -o hello.wasm \
-  hello.c
